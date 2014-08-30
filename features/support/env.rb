ce28a3e2f93952a602d29db5ab8c903c6ecc36f2
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-08-30 15:16:39 +0200

    Furhtering the passwordless world

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,14 +1,15 @@
 require_relative "../../src/memory_identity_store.rb"
 require_relative "../../src/mailgun_mail_sender.rb"
+require_relative "../../src/emailer.rb"
 
 class PasswordlessLocalWorld
   def initialize
-    store = MemoryIdentityStore.new
+    identity_store = MemoryIdentityStore.new
 
     mail_sender = MailgunMailSender.new "key-3ax6xnjp29jd6fds4gc373sgvjxteol0", "samples.mailgun.org", "integration test <integration-test@deranged.dk>"
-    emailer = Emailer.new mail_sender
+    emailer = Emailer.new mail_sender message_store
 
-    @identity_service = IdentityService.new store, emailer 
+    @identity_service = IdentityService.new identity_store, emailer 
   end
 
   def identity_service
